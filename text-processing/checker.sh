4fbe03bf0cddf27d8d6290ee22c8fbe8295b82a2
thsh77 <thsh77@protonmail.com>
2020-01-07 20:01:55 +0100

    improvements

--- text-processing/checker.sh
+++ text-processing/checker.sh
@@ -6,22 +6,32 @@
 PROGRAM=`basename "$@"`
 VERSION='1.0'
 CURRENT_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TMP_DIR=`mktemp -d -p "$CURRENT_DIR"`
-SOURCE_DIR='./'
-TARGET_DIR='./'
+SCRATCHDIR=`mktemp -d -p "$CURRENT_DIR"`
+SOURCEDIR='./'
+TARGETDIR='~/Desktop/'
 STYLESHEET='/home/th/Development/textbase/tekstnet-factory/generator/xsl/generator.xsl'
 
 transform() {
   java -cp /usr/local/lib/saxon/saxon9he.jar net.sf.saxon.Transform \
     -s:"$1" \
     -xsl:$STYLESHEET \
-    -o:"$TMP_DIR/test"
+    -o:"$SCRATCHDIR/test"
 }
 
-#if [ -d "$TARGET_DIR$(basename "$1" .txt)" ]
-#then
-#  echo "Directory exists at " "$TARGET_DIR$(basename "$1" .txt)"
-#fi
+cleanup() {
 
-transform $1
+  dirname=`ls $SCRATCHDIR`
+  
+  if [ -d "/home/th/Desktop/$dirname" ]
+  then
+    echo The directory $dirname already exists -- doing rsync
+    rsync $SCRATCHDIR/$dirname/* /home/th/Desktop/$dirname/
+  else
+    echo Make new dir
+    cp -r $SCRATCHDIR/$dirname /home/th/Desktop/
+  fi
 
+  rm -r $SCRATCHDIR;
+}
+
+transform $1 && cleanup 
