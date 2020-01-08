9991bf8040b1c502c64115214c3edef301777a6e
thsh77 <thsh77@protonmail.com>
2020-01-08 19:52:47 +0100

    modified script

--- text-processing/tirsdagBookFactory.sh
+++ text-processing/tirsdagBookFactory.sh
@@ -8,7 +8,7 @@ VERSION='1.0'
 CURRENT_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SCRATCHDIR=`mktemp -d -p "$CURRENT_DIR"`
 SOURCEDIR='./'
-TARGETDIR='~/Desktop/'
+TARGETDIR='/home/th/Development/tirsdagsprojektet/content/books'
 STYLESHEET='/home/th/Development/textbase/tekstnet-factory/generator/xsl/generator.xsl'
 
 
@@ -22,15 +22,16 @@ transform() {
 
 cleanup() {
 
+  #Get directory name from temporary dir
   dirname=`ls $SCRATCHDIR`
   
-  if [ -d "/home/th/Desktop/$dirname" ]
+  if [ -d "$TARGETDIR/$dirname" ]
   then
     echo The directory $dirname already exists -- doing rsync
-    rsync -av $SCRATCHDIR/$dirname/* /home/th/Desktop/$dirname/
+    rsync -av $SCRATCHDIR/$dirname/* $TARGETDIR/$dirname/
   else
     echo Make new dir
-    cp -r $SCRATCHDIR/$dirname /home/th/Desktop/
+    cp -r $SCRATCHDIR/$dirname $TARGETDIR
   fi
 
   rm -r $SCRATCHDIR;
