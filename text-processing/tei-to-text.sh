9cbe068c02919297cac213a41b94e5b817b2d40e
thsh77 <thsh77@protonmail.com>
2020-02-26 11:12:23 +0100

    Added the tei-to-text script

---
+++ text-processing/tei-to-text.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# tei-to-text extracts text from a TEI P5 file in simple text form
+
+transform() {
+
+  # First map tei prefix to namespace, then match every div element
+  # and then all relevant content subelements and output its textual
+  # content wrapped in newlines.
+
+  xmlstarlet sel \
+    -N tei="http://www.tei-c.org/ns/1.0" \
+    -t -m '//tei:div' \
+    -m 'tei:head | tei:p | tei:lg | tei:note | tei:figure | tei:app' -n -v '.' -n \
+  $i
+}
+
+cleanup() {
+  
+  # In case there are multiple blank lines these are to be reduced 
+  # to one. Use par to format paragraphs
+  
+  sed ':a; /^\n*$/{ s/\n//; N;  ba};'
+}
+
+for i in "$@"
+do
+  transform | cleanup | par 
+done
+
