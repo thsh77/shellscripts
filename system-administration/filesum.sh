ed68163c43c3a4b368e047fe86d5bdfdab701c99
thsh77 <thsh77@protonmail.com>
2020-10-12 17:28:25 +0200

    Add printf

--- system-administration/filesum.sh
+++ system-administration/filesum.sh
@@ -6,27 +6,27 @@ ls -l $* | awk '
 # input: long listing produced by "ls -l"
 
 # 1 output column headers
-BEGIN { print "BYTES" , "\t", "FILE" }
+BEGIN { printf("FILE\tBYTES\n") }
 
 # 2 test for 9 fields; files begin with "-"
 NF == 9 && /^-/ {
         sum += $5
         ++filenum
-        print $5, "\t", $9
+        printf("%-15s\t%10d\n", $9, 55)
 }
 
 # 3 test for 9 fields; directory begins with "d"
 NF == 9 && /^d/ {
-        print "<dir>", "\t", $9
+        printf("<dir>\t%10d\n", $9)
 }
 
 # 4 test for ls -lR line ./dir:
 $1 ~ /^\..*:$/ {
-        print "\t" $0
+        printf("\t%10d\n" $0)
 }
 
 # 5 once all is done
 END {
         # print total file size and number of files
-        print "Total: ", sum, "bytes (" filenum " files)"
+        printf("Total: %d bytes (%d files)\n", sum, filenum)
 }'
