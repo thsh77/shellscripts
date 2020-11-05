dd068661c240f477ccf7201f58fc87382759ad2c
thsh77 <thsh77@protonmail.com>
2020-11-05 19:30:30 +0100

    Progress

--- text-processing/esproc/esproc.sh
+++ text-processing/esproc/esproc.sh
@@ -5,24 +5,56 @@
 #
 
 
-awk '
+make_index () {
 
-function basename(file, a, n){
-  n = split(file, a, "/")
-  id = a[1]
-  file = a[n]
-#  return id file
-  printf("id = %s\n file = %s", id, file)
+  awk '
+
+  function document_id(file, a, n){
+    n = split(file, a, "/")
+    work_id = a[n-1]
+    filename = a[n]
+    sub(".html", "", filename)
+    printf("  \"id\" : \"%s_%s\"", work_id, filename)
+  }
+
+
+  /title/ {
+    print 'title' 
+  }
+  
+  #END { document_id($file) }
+  '
 }
 
-END {
-#  f = basename(FILENAME)
-#  {print f }
+#find $1 -type f -name '_index.md'
+#while read file
+#do
+#  #title=$(awk '/title/ {print $0}' $file)
+#  echo "HEJ"
+#done
 
-{ print basename(FILENAME) }
-#{ print FILENAME }
-#{ print "id = " id }
-#{ print id }
-}
+worktitle=$(awk '
+  BEGIN { FS = ":" }
+  /^title/ {print "\"title\" : "$2 }
+  ' $1/_index.md)
 
-' $1
+find $1 -type f -name '*[0-9].html' | sort |
+while read file
+do 
+  echo "doc$((i++))"" = {"
+  echo $worktitle
+  awk '
+  BEGIN { FS = ":" }
+    /^title/ { print "\"chapter\" : "$2 }
+    ' $file
+  #cat $1/_index.md
+  #make_index
+  printf "\n}\n"
+done
+
+echo $te
+#find $1 -type f -name '_index.md'
+#while read file
+##do
+#  echo HEJ
+#done
