3d15690deb39418beb4c807d76e254522895cb3e
thsh77 <thsh77@protonmail.com>
2020-12-10 16:42:39 +0100

    Improvement

--- text-processing/esproc/esproc.sh
+++ text-processing/esproc/esproc.sh
@@ -1,40 +1,26 @@
 #!/bin/bash
-#
-# esproc - process yaml/html file for elasticsearch indexing
-# Usage: ./esproc.sh brahe-t_de-nova-stella/ | jq -c . > text.json
-#
-#
 
 
-shopt -s extglob
+c=1     #Set a counter
 
-x=0
+for f in $(find . -type f -name "*[0-9].html")
 
-for dir in $@
 do
-  find $dir -type f -name '*[0-9].html' | sort |
+  #echo "$(basename $f)"
+  #printf "%06d\n" $c 
+  path=$(           dirname ${f##./})/$(basename ${f%.*})
+  work_id=$(        dirname ${f##./})_$(basename ${f%.*})
+  work_title=$(     awk -F ":" '/^title:/ {print $2}' `dirname ${f}`/_index.md)
+  chapter_title=$(  awk -F ":" '/^title/ {print $2}' $f)
+  view=$(awk -v RS='(<[^>]+>|\\||{{% images/figure %}})' -v ORS=' ' 'NR > 5 {m=1; gsub(/[ \t\n]+/, " ")}m' $f)
+  printf "{\"index\":{ \"_id\" : %06d}}" $c
+  printf "\n{"
+  printf "\t\"%s\":\"%s\"," path $path
+  printf "\t\"%s\":\"%s\"," work_id $work_id
+  printf "\t\"%s\":%s," work_title "$work_title"
+  printf "\t\"%s\":%s," chapter_title "$chapter_title"
+  printf "\t\"%s\":%s" view \""$view"\"
+  printf "\n}\n"
+  ((c++)) 
 
-    while read file
-    do
-      x=$(($x+1))
-      # Count the docs, but start from 1
-      #echo "doc$((i+++1))"" = {"
-      echo "Sidetal: " $x
-
-      work_id=$(        dirname ${file##./})_$(basename ${file%.*})
-      work_title=$(     awk -F ":" '/^title:/ {print $2}' $1/_index.md)
-      chapter_title=$(  awk -F ":" '/^title/ {print $2}' $file)
-      text_block=$(     awk '/<div/,0 {printf $0}' $file)
-      view=$(awk -v RS='<[^>]+>' -v ORS='' 'NR > 5 {m=1; gsub(/[ \t\n]+/, " ")}m' $file)
-      #printf "{\"index\":{ \"_id\" : \"$((i+++1))\"}}"
-      printf "{\"index\":{ \"_id\" : \"$x\"}}"
-      printf "\n{"
-      printf "\t\"%s\":\"%s\"," work_id $work_id
-      printf "\t\"%s\":%s," work_title "$work_title"
-      printf "\t\"%s\":%s," chapter_title "$chapter_title"
-      printf "\t\"%s\":%s" view \""$view"\"
-      printf "\n}\n"
-    done
 done
-
-
