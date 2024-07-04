d7d19a905dc454267bc7ec32c77adb4e1bac2e5c
thsh77 <thsh77@protonmail.com>
2024-07-04 13:03:18 +0200

    Add tsvnotes2toml.sh

---
+++ toml/tsvnotes2toml.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##################################################################
+#
+# tsv2toml is designed for transforming a tsv file to a TOML hash
+# table. The input file has to look like this:
+#
+#    lemmatext1  explanationtext1
+#    lemmatext2  explanationtext2
+#
+# to render this
+#
+#    [n1]
+#     lemma = "lemmatext1"
+#     note= "explanationtext1"
+#
+#    [n2]
+#     lemma = "lemmatext2"
+#     note= "explanationtext2"
+#
+##################################################################
+
+
+awk -v FS='\t' -v s1="\"" '
+BEGIN {n=1}
+{
+  print "[n"n"]"
+  print "  lemma = "s1 $1 s1
+  print "  note  = "s1 $2 s1
+  {print ""}
+  {n++}
+}' "$@"
